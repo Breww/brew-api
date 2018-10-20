80b947c1d30d0224da9d9cc4ba9d1b8b9db19ddb
NickLewanowicz <nicklewanowicz@gmail.com>
2018-10-20 19:22:56 -0400

    Fix import

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
 export { recommendPersonal } from './engines/recommendations/personalized';
 export { recommendCategorical } from './engines/recommendations/categorical';
+export { getAutoComplete } from './engines/getAutoComplete';
 export { getBeerImage } from './engines/getBeerImage';
