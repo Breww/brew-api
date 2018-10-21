3a2a0e2d0514f3d1487062164d5fb8cddda36748
Seena Rowhani <seenarowhani95@gmail.com>
2018-10-21 00:22:53 -0400

    chore(clean): dasherize filenames

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,6 +1,6 @@
 export { recommendPersonal } from './engines/recommendations/personalized';
 export { recommendCategorical } from './engines/recommendations/categorical';
-export { getAutoComplete } from './engines/getAutoComplete';
-export { getBeerImage } from './engines/getBeerImage';
+export { getAutoComplete } from './engines/get-auto-complete';
+export { getBeerImage } from './engines/get-beer-image';
 export { ocr } from './engines/ocr';
 export { ocrBuffer } from './engines/ocr-buffer';
